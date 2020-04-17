1b1cc5fbcf9b8bb048749b138d3226babfd2f50e
jiakaiqiang <jkq19961125@>
2020-04-17 10:47:43 +0800

    11

--- 项目/shopping-admin-web-master/shopping-admin-web-master/config/dev.env.js
+++ 项目/shopping-admin-web-master/shopping-admin-web-master/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://120.27.63.9:8080"'
 })
